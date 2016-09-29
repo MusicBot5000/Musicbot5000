7bc66a4e2acb777a9f31a4894f1274a3659c7e80
PaulJSchellenberg <pauljschellenberg@gmail.com>
2016-09-29 10:23:59 -0500

    Commenting Comms.cs
    Commenting the code to help others work off of the code

--- ReferenceCode/Unity-Ardu_CommTest/Comms.cs
+++ ReferenceCode/Unity-Ardu_CommTest/Comms.cs
@@ -6,13 +6,16 @@ public class Comms : MonoBehaviour {
     SerialPort serial;
     // Use this for initialization
     void Start () {
-        serial = new SerialPort("COM7",9600);
-        Debug.Log("hello");
+        serial = new SerialPort("COM7",9600); // My arduino was connected to COM7 change this for you guys
+	    				      // baud rate has to match ardu
+	    
+        Debug.Log("hello"); // check to see if this script is running in unity by seeing if the console displays this
 	}
 	
 	// Update is called once per frame
 	void Update () {
         
+		//If the up arrow key is pressed, send "hello" to ardu. You can play with what I send.
         if (Input.GetKey(KeyCode.UpArrow))
         {
             serial.Open();
