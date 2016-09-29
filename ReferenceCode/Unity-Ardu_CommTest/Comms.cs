ee1b46136137b66c949d275eabc31ca08c8bb5e8
PaulJSchellenberg <pauljschellenberg@gmail.com>
2016-09-29 10:13:53 -0500

    Add files via upload

---
+++ ReferenceCode/Unity-Ardu_CommTest/Comms.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class Comms : MonoBehaviour {
+    SerialPort serial;
+    // Use this for initialization
+    void Start () {
+        serial = new SerialPort("COM7",9600);
+        Debug.Log("hello");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            serial.Open();
+            serial.Write("hello");
+            serial.Close();
+        }
+	}
+}
---
+++ ReferenceCode/Unity-Ardu_CommTest/sketch_sep27a.ino
@@ -0,0 +1,28 @@
+int incomingByte = 0;   // for incoming serial data
+
+int hello =12;
+int bye = 3;
+int brake = 9;
+
+void setup() {
+        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+        pinMode(hello,OUTPUT);
+        pinMode(bye,OUTPUT);
+        pinMode(brake,OUTPUT);
+        
+        digitalWrite(hello,HIGH);
+        digitalWrite(brake,LOW);
+}
+
+void loop() {
+
+        // send data only when you receive data:
+        if (Serial.available() > 0) {
+                // read the incoming byte:
+                Serial.read();
+                digitalWrite(bye,HIGH);
+                delay(9);
+                digitalWrite(bye,LOW);
+        }
+                
+}
