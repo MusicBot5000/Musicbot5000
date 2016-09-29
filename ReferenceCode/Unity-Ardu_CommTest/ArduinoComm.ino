35c7dc27674333a975e9a9fe440f5287d89aafc4
PaulJSchellenberg <pauljschellenberg@gmail.com>
2016-09-29 10:20:39 -0500

    Commenting Arduino.ino
    I commented this code to make it easier for others to work off of.

--- ReferenceCode/Unity-Ardu_CommTest/ArduinoComm.ino
+++ ReferenceCode/Unity-Ardu_CommTest/ArduinoComm.ino
@@ -1,11 +1,14 @@
 int incomingByte = 0;   // for incoming serial data
 
+//These variables are required for the motorshield.
 int direction =12;
 int solenoid = 3;
 int brake = 9;
 
 void setup() {
         Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+        // might be good to look into higher baud rates? i don't know
+        
         pinMode(direction,OUTPUT);
         pinMode(solenoid,OUTPUT);
         pinMode(brake,OUTPUT);
@@ -19,7 +22,11 @@ void loop() {
         // send data only when you receive data:
         if (Serial.available() > 0) {
                 // read the incoming byte:
-                Serial.read();
+                
+                Serial.read(); // I read serial to empty the buffer. I don't do anything with the value. 
+                               // You will be able to read the bytes unity sends you through this command
+                
+                // Engage and disengage the solenoid.
                 digitalWrite(solenoid,HIGH);
                 delay(9);
                 digitalWrite(solenoid,LOW);
