349e5223ce99b47cd2d630816dd3e780c9e4f7d4
BenjaminJWhite <whiteb36@myumanitoba.ca>
2016-10-15 19:35:14 -0500

    Added a Xylophone model that prints notes hit to console

---
+++ VR/MusicBot 5000/Assets/Instruments/Scripts/InstrumentController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstrumentController : MonoBehaviour {
+    public string n;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void PlayNote(string note)
+    {
+        n = note;
+        Debug.Log("Note " + note + " played.");
+    }
+}
---
+++ VR/MusicBot 5000/Assets/Instruments/Scripts/NoteController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoteController : MonoBehaviour {
+
+    public string note;
+    InstrumentController cont;
+	// Use this for initialization
+	void Start () {
+        cont = GameObject.FindGameObjectWithTag("Instrument").GetComponent<InstrumentController>() ;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            cont.SendMessage("PlayNote", note);
+        }
+    }
+}
