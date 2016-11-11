660a5fc10612e4ef790bcd78e9b77daf30b17fb0
BenjaminJWhite <whiteb36@myumanitoba.ca>
2016-11-11 15:10:17 -0600

    Adding in drum and Instrument Selection Functionality
    Now you can make a Xylophone!

---
+++ VR/MusicBot 5000/Assets/Game-Controller/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    // Use this for initialization
+    public Transform InstrumentSpot;
+    public GameObject[] Instruments;
+    GameObject InstrumentInst;
+
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ChangeInstrument(int Id)
+    {
+        if (InstrumentInst != null)
+        {
+            Destroy(InstrumentInst);
+        }
+        if (Id >= 0)
+        {
+            InstrumentInst = Instantiate(Instruments[Id], InstrumentSpot) as GameObject;
+        }
+    }
+}
--- VR/MusicBot 5000/Assets/Hands/Scripts/optionToggler.cs
+++ VR/MusicBot 5000/Assets/Hands/Scripts/optionToggler.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class optionToggler : MonoBehaviour {
 
-
+    public int Instrument;
     public Material UnSelected;
     public Material Selected;
     SerialPort serial;
@@ -15,18 +15,21 @@ public class optionToggler : MonoBehaviour {
     Material MeMat;
     Material O1Mat;
     Material O2Mat;
+    GameController GameCon;
 
     // Use this for initialization
     void Start () {
         MeMat = Me.GetComponent < Renderer >().material;
         O1Mat = Other1.GetComponent<Renderer>().material;
         O2Mat = Other2.GetComponent<Renderer>().material;
+        GameCon = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("MenuPointer"))
         {
+            GameCon.ChangeInstrument(Instrument);
             MeMat.color = Selected.color;
             O1Mat.color = UnSelected.color;
             O2Mat.color = UnSelected.color;
