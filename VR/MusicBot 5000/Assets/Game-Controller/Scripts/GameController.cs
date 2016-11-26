f2f8557e1c5fe4241bc5e0ad013d8a7ee7c8b01a
PaulJSchellenberg <pauljschellenberg@gmail.com>
2016-11-25 19:55:23 -0600

    mallet and drumstick  models
    made mallet and drumstick models
    mapped to finger and fist
    added hand gesture logic

---
+++ VR/MusicBot 5000/Assets/Game-Controller/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+
+    // Use this for initialization
+    public Transform InstrumentSpot;
+    public GameObject[] Instruments;
+    GameObject InstrumentInst;
+    public int InstrumentID;
+
+
+    void Start()
+    {
+        InstrumentID = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeInstrument(int Id)
+    {
+        if (InstrumentInst != null && Id != InstrumentID)
+        {
+            Destroy(InstrumentInst);
+        }
+        if (Id >= 0 && Id != InstrumentID)
+        {
+            InstrumentInst = Instantiate(Instruments[Id], InstrumentSpot) as GameObject;
+        }
+        InstrumentID = Id;
+    }
+}
---
+++ VR/MusicBot 5000/Assets/Hands/Scripts/fingerStickToggler.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fingerStickToggler : MonoBehaviour
+{
+    
+    public GameObject Hand;
+    public GameObject Finger;
+    public GameObject Stick;
+    private GameObject Tip, Knuckle;
+    private Vector3 FingerVector, KnuckleVector;
+
+    // Use this for initialization
+    void Awake()
+    {
+    }
+
+    void Start()
+    {
+        Tip = Finger.transform.FindChild("bone3").gameObject;
+        Knuckle = Finger.transform.FindChild("bone1").gameObject;
+
+        FingerVector = Tip.transform.position - Knuckle.transform.position;
+        Stick.transform.rotation = Quaternion.LookRotation(FingerVector.normalized);
+        Stick.transform.position = Knuckle.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FingerVector = Tip.transform.position - Knuckle.transform.position;
+        Stick.transform.rotation = Quaternion.LookRotation(FingerVector.normalized);
+        KnuckleVector = Knuckle.transform.position;
+        KnuckleVector.z -= 0.11f;
+        Stick.transform.position = KnuckleVector;
+    }
+
+    public void ShowStick()
+    {
+        StartCoroutine(ActivateStick(true));
+    }
+
+    public void HideStick()
+    {
+        StartCoroutine(ActivateStick(false));
+    }
+
+    IEnumerator ActivateStick(bool active)
+    {
+        Hand.SetActive(!active);
+        Stick.SetActive(active);
+        yield return null;
+    }
+}
