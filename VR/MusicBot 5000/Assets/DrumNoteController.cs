3f1ab57946e581dac8ab140d8a64351ff96fc1b2
BenjaminJWhite <whiteb36@myumanitoba.ca>
2017-01-27 14:26:34 -0600

    Drum Roll Fixes
    Now the drum roll is VR-side.

--- VR/MusicBot 5000/Assets/DrumNoteController.cs
+++ VR/MusicBot 5000/Assets/DrumNoteController.cs
@@ -4,36 +4,47 @@ using System.Collections;
 public class DrumNoteController : NoteController {
     
     public float delay;
-    public string endRoll;
-    private float startTime;
+    public string note2;
+    private float nextTime;
+    public float nextRoll;
+    private bool roll1;
 
 	// Use this for initialization
 	new void Start () {
         base.Start();
+        roll1 = true;
 	}
 
     new void OnTriggerEnter(Collider col)
     {
         base.OnTriggerEnter(col);
-        startTime = Time.unscaledTime;
+        nextTime = Time.unscaledTime+delay;
     }
 
 	void OnTriggerStay(Collider col)
     {
-        if(Time.unscaledTime - startTime > delay)
+        if(Time.unscaledTime > nextTime)
         {
             if (col.gameObject.tag == "Player")
             {
-                cont.SendMessage("PlayNote", note);
+                nextTime = Time.unscaledTime + nextRoll;
+                if (roll1)
+                {
+                    cont.SendMessage("PlayNote", note);
+                }else
+                {
+                    cont.SendMessage("PlayNote", note2);
+                }
+                roll1 = !roll1;
             }
         }
     }
 
-    void OnTriggerExit(Collider col)
-    {
-        if(col.gameObject.tag == "Player")
-        {
-            cont.SendMessage("PlayNote", endRoll);
-        }
-    }
+    //void OnTriggerExit(Collider col)
+    //{
+    //    if(col.gameObject.tag == "Player")
+    //    {
+    //        cont.SendMessage("PlayNote", endRoll);
+    //    }
+    //}
 }
